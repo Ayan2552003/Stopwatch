0b85e427e7f0f285b84dd44a1d5f476e7cea59ca
Syed Ayan Adeel <ayanadeelpak@gmail.com>
2021-05-21 23:54:51 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,48 @@
+var hourHead = document.getElementById('hour')
+var minHead = document.getElementById('min')
+var secHead = document.getElementById('sec')
+var msecHead = document.getElementById('msec')
+var btndis = document.getElementById('disabled')
+var hour = 00;
+var min = 00;
+var sec = 00;
+var msec = 00;
+var interval;
+
+
+function timer() {
+    msec++
+    msecHead.innerHTML = msec
+    if (msec == 100) {
+        sec++
+        secHead.innerHTML = sec
+        msec = 00
+    } else if (sec == 10) {
+        min++
+        minHead.innerHTML = min
+        sec = 00
+    } 
+}
+
+function start() {
+    interval = setInterval(timer, 10)
+    btndis.disabled = true
+}
+
+function pause() {
+    clearInterval(interval)
+    btndis.disabled = false
+}
+
+function reset() {
+    msec = 00;
+    sec = 00;
+    min = 00;
+
+    msecHead.innerHTML = msec;
+    secHead.innerHTML = sec;
+    minHead.innerHTML = min;
+
+    clearInterval(interval)
+    btndis.disabled = false
+}
